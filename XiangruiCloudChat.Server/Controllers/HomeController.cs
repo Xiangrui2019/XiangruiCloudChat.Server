65035445b31f7f7580403f4c746f73759b030478
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-03-18 12:44:10 +0800

    Add Index Page And Test Ping

---
+++ XiangruiCloudChat.Server/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Aiursoft.Pylon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using XiangruiCloudChat.Server.Services;
+
+namespace XiangruiCloudChat.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return this.ChatJson(new AiurProtocol
+            {
+                Code = ErrorType.Success,
+                Message = $"欢迎访问祥瑞云易信{_configuration["CurrentVersion"]}的服务器端, 但是, 除了祥瑞云易信的客户端和SDK, 不可以使用浏览器直接访问."
+            });
+        }
+
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
--- XiangruiCloudChat.Server/Startup.cs
+++ XiangruiCloudChat.Server/Startup.cs
@@ -33,8 +33,6 @@ namespace XiangruiCloudChat.Server
             Mode = Convert.ToBoolean(configuration["LaxCookie"]) ? SameSiteMode.Lax : SameSiteMode.None;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureLargeFileUpload();
@@ -60,7 +58,6 @@ namespace XiangruiCloudChat.Server
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
