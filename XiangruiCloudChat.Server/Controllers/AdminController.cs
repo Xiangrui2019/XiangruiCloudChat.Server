9743f21299ef2c6f659980287750e120b1f9307b
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-03-24 13:58:55 +0800

    Add Admin APIS Base Controller

---
+++ XiangruiCloudChat.Server/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mime;
+using Aiursoft.Pylon.Attributes;
+using Aiursoft.Pylon.Services.ToAPIServer;
+using Microsoft.AspNetCore.Mvc;
+using XiangruiCloudChat.Server.Data;
+
+namespace XiangruiCloudChat.Server.Controllers
+{
+    [APIExpHandler]
+    [APIModelStateChecker]
+    [AiurForceAuth(directlyReject: true)]
+    public class AdminController : Controller
+    {
+    }
+}
